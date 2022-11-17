db0f93bc0019c57fd9c4b5d6ccba21aa9616c153
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-11-17 15:26:50 +0000

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -57,7 +57,7 @@ const Portfolio = () => {
           {/* <p className="py-6">Check out some of my work right here</p> */}
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-5 px-12 sm:px-0">
           {portfolios.map(({ id, src, url, code,pname,tech }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
